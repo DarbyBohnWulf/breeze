927ab809492b5acae43a986ebeb3cda21b50d6af
Gregory Leung <not.gregs.other.email@gmail.com>
2019-10-25 12:47:07 -0500

    Rendered user login route

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const userController = require('./controllers/userController.js')
+app.use('/users', userController)
+
 app.listen(PORT, () => {
     console.log(`Started server on port ${PORT}`)
 })
