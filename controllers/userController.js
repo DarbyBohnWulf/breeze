3cf32d0de09b6ecdb5eae37ef6344cc631bc7f84
Gregory Leung <not.gregs.other.email@gmail.com>
2019-10-25 13:09:44 -0500

    Stored passwords securely

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/user.js')
 
@@ -13,14 +14,18 @@ router.post('/login', (req, res, next) => {
 
 router.post('/register', async (req, res, next) => {
     const username = req.body.username
-
+    const hashedPass = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+    
     try {
         const foundUser = await User.findOne({username: username})
         if(foundUser) {
             res.send('already taken')
         } else {
-            User.create(req.body)
-            res.send('created')
+            User.create({
+                username: username,
+                password: hashedPass,
+            })
+            res.send('created securely')
         }
     } catch (err) {
         res.json(err)
