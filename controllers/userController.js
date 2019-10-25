46e886cb2b1a317aa5182ad1ab6800f192bc6ba9
Gregory Leung <not.gregs.other.email@gmail.com>
2019-10-25 12:56:12 -0500

    Added login and register routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../models/user.js')
+
 router.get('/login', (req, res) => {
     res.render('users/login.ejs')
+})
+
+router.post('/login', (req, res, next) => {
+    res.redirect('/users/login')
+})
+
+router.post('/register', (req, res, next) => {
+    console.log(req.body)
 })
 
 module.exports = router
