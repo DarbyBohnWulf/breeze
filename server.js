7dbff81e59ddcede69e6b673008b43498a7850f2
Gregory Leung <not.gregs.other.email@gmail.com>
2019-10-25 12:55:44 -0500

    Used urlencoded to parse request body0

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.use(express.urlencoded({ extended: false }))
+
 const userController = require('./controllers/userController.js')
 app.use('/users', userController)
 
