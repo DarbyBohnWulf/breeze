be23b3d12394a3597f4b7fc4f39c0dcd80ece7f7
Gregory Leung <not.gregs.other.email@gmail.com>
2019-10-28 07:00:33 -0500

    Created garment to card UI utility

--- public/js/app.js
+++ public/js/app.js
@@ -18,13 +18,38 @@ const closetUiController = {
     clothes: [],
 
     populateCategory: function(garmentRole) {
-
+        this.clothes.forEach((g) => {
+            this.addCardToRole(garmentRole, g)
+        })
     },
 
     //Givena role and garment object, creates a card
     //to place in the closet section
-    addCard: function(garmentRole, garment) {
+    addCardToRole: function(garmentRole, garment) {
+        const $card = this.getGarmentCard(garment)
+        $(`#${garmentRole}`).append($card)
+    },
 
+    //Given a garment object, creates and returns a
+    //bootstrap card element
+    getGarmentCard: function(garment) {
+        const $card = $('<div>').addClass('card garment')
+
+        const $imageCap = $('<img>').addClass('card-img-top')
+        $imageCap.attr('src', 'https://picsum.photos/id/461/200/200')
+        $imageCap.attr('alt', 'Card image cap')
+        $card.append($imageCap)
+
+        const $cardBody = $('<div>').addClass('card-body')
+        $card.append($cardBody)
+
+        const $title = $('<h5>').text(garment.name)
+        $cardBody.append($title)
+
+        const $garmentAttributes = $('<p>').text(`${garment.role} ${garment.layer} ${garment.precip}`)
+        $cardBody.append($garmentAttributes)
+
+        return $card
     },
 }
 
@@ -33,7 +58,12 @@ $(document).ready(async () => {
     try {
         const userClothes = await apiInterface.getCloset()
         closetUiController.clothes = userClothes
+        //update UI here
     } catch (err) {
         console.log(err)
     }
+})
+
+$(document).on('click', async (e) => {
+    console.log(e.target.id)
 })
