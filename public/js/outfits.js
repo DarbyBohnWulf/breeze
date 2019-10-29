59554e6b2158368275ad23da0726f100a86a37b9
Gregory Leung <not.gregs.other.email@gmail.com>
2019-10-29 11:38:51 -0500

    render options in the create an outfit

--- public/js/outfits.js
+++ public/js/outfits.js
@@ -1,6 +1,6 @@
 const outfitsUiController = {
   getOutfitCard: function(outfit) {
-    const $card = $('<div>').addClass('card garment').attr('_id', outfit._id)
+    const $card = $('<div>').addClass('card').attr('_id', outfit._id)
 
     const $ul = $('<ul>')
     outfit.garments.forEach(g => {
@@ -9,6 +9,24 @@ const outfitsUiController = {
     })
     $card.append($ul)
 
+    const $deleteButton = $('<a>').addClass('btn btn-danger').text('Delete')
+    $card.append($deleteButton)
+    return $card
+  },
+
+  getBlankOutfitCard: function() {
+    const $card = $('<div>').addClass('card')
+
+    const $ul = $('<ul>').appendTo($card)
+    const $addGarmentLi = $('<li>').appendTo($ul)
+    const $garmentSelect = $('<select>').appendTo($addGarmentLi)
+    //populate options
+    apiInterface.getCloset().then((res) => {
+      res.forEach(g => {
+        const $option = $('<option>').attr('value', g._id).text(g.name).appendTo($garmentSelect)
+      })
+    })
+    const $garmentConfirm = $('<a>').addClass('btn btn-success').text('+').appendTo($addGarmentLi)
     return $card
   }
 }
@@ -21,4 +39,8 @@ $(document).ready(async () => {
   } catch (err) {
     console.log(err)
   }
+})
+
+$('#new').on('click', () => {
+  const $blankOutfit = outfitsUiController.getBlankOutfitCard().appendTo('#outfits')
 })
