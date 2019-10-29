85f67df0b5106e6b5b0412a6bacdac3548d133d1
Gregory Leung <not.gregs.other.email@gmail.com>
2019-10-29 14:23:43 -0500

    Set up POST for api/closet/outfits

--- controllers/closetController.js
+++ controllers/closetController.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
+const Outfit = require('../models/outfit.js')
+
 router.get('/', (req, res, next) => {
     req.session.test = "hi"
     console.log("\nhere is req.session in GET /closet", req.session)
     res.render('closet/index.ejs')
 })
 
-router.get('/outfits/', (req, res, next) => {
+router.get('/outfits', (req, res, next) => {
     res.render('closet/outfits.ejs')
+})
+
+router.post('/outfits', async (req, res, next) => {
+    try {
+      console.log(req.body)
+      const createdOutfit = await Outfit.create(req.body)
+      res.json(createdOutfit)
+    } catch (err) {
+      next(err)
+    }
 })
 
 module.exports = router
--- public/js/app.js
+++ public/js/app.js
@@ -1,56 +1,63 @@
 const apiInterface = {
-    URL: "http://localhost:3000/api/garments",
-    outfitsURL: "http://localhost:3000/api/outfits",
+  URL: "http://localhost:3000/api/garments",
+  outfitsURL: "http://localhost:3000/api/outfits",
 
-    //Gets all garments from user's closet and populates
-    //closetGarments
-    //User should be logged in, or else this may break
-    getCloset: async function() {
-        try {
-            const res = await $.getJSON(`${this.URL}`)
-            return res
-        } catch (err) {
-            console.log(err)
-        }
-    },
-
-    createGarment: async function(garment) {
-        try {
-            const res = await $.post(`${this.URL}`, garment)
-            return res
-        } catch (err) {
-            console.log(err)
-        }
-    },
-
-    //expects a garment with _id field so that server can update
-    updateGarment: async function(garment){
-        try {
-            //Make a post request to domain/api/closet/garmentId?_method=PUT
-            const res = await $.post(`${this.URL}/${garment._id}?_method=PUT`, garment)
-            return res
-        } catch (err) {
-            console.log(err)
-        }
-    },
-
-    deleteGarment: async function(garment){
-        try {
-            const res = await $.post(`${this.URL}/${garment._id}?_method=DELETE`)
-            return res
-        } catch (err) {
-            console.log(err)
-        }
-    },
-
-    getOutfits: async function() {
-        try {
-            const res = await $.getJSON(`${this.outfitsURL}`)
-            return res
-        } catch (err) {
-            console.log(err)
-        }
+  //Gets all garments from user's closet and populates
+  //closetGarments
+  //User should be logged in, or else this may break
+  getCloset: async function() {
+    try {
+      const res = await $.getJSON(`${this.URL}`)
+      return res
+    } catch (err) {
+      console.log(err)
     }
+  },
+
+  createGarment: async function(garment) {
+    try {
+      const res = await $.post(`${this.URL}`, garment)
+      return res
+    } catch (err) {
+      console.log(err)
+    }
+  },
+
+  //expects a garment with _id field so that server can update
+  updateGarment: async function(garment){
+    try {
+      //Make a post request to domain/api/closet/garmentId?_method=PUT
+      const res = await $.post(`${this.URL}/${garment._id}?_method=PUT`, garment)
+      return res
+    } catch (err) {
+      console.log(err)
+    }
+  },
+
+  deleteGarment: async function(garment){
+    try {
+      const res = await $.post(`${this.URL}/${garment._id}?_method=DELETE`)
+      return res
+    } catch (err) {
+      console.log(err)
+    }
+  },
+
+  getOutfits: async function() {
+    try {
+      const res = await $.getJSON(`${this.outfitsURL}`)
+      return res
+    } catch (err) {
+      console.log(err)
+    }
+  },
+
+  createOutfit: async function(outfit) {
+    try {
+      const res = await $.post(this.outfitsURL, outfit)
+      return res
+    } catch (err) {
+      console.log(err)
+    }
+  }
 }
-
-
