142b2de6d0fb5d13d957f46c8a13c6d3fd4cf145
Mark Ritchie <mwritchie2@gmail.com>
2019-10-29 23:50:45 -0500

    fixed formatting

--- models/user.js
+++ models/user.js
@@ -1,26 +1,26 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String, 
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    closet: [{
-        type: mongoose.Schema.Types.ObjectId,
-        refs: 'Garment'
-    }],
-    outfits: [{
-        type: mongoose.Schema.Types.ObjectId,
-        refs: 'Outfit'
-    }],
-    ootd: [{
-        type: mongoose.Schema.Types.ObjectId,
-        refs: 'Garment'
-    }],
+  username: {
+    type: String, 
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  closet: [{
+    type: mongoose.Schema.Types.ObjectId,
+    refs: 'Garment'
+  }],
+  outfits: [{
+    type: mongoose.Schema.Types.ObjectId,
+    refs: 'Outfit'
+  }],
+  ootd: [{
+    type: mongoose.Schema.Types.ObjectId,
+    refs: 'Garment'
+  }],
 })
 
 const User = mongoose.model('User', userSchema)
